6a361226b54985a8ca9f77c32ac5930b05efdca0
Arseni <arseni.natapov@gmail.com>
2025-01-25 18:08:54 +0300

    Added buff board component

---
+++ Blackboard/Buffs/BoardBuff.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+namespace BB
+{
+	public sealed class BoardBuff : BaseBoardKey
+	{
+		[SerializeField]
+		SerializedBoardValues _values = new();
+		public override IBoardValueContainer CreateValue(IBoard board)
+			=> new BoardValue(this, board);
+		sealed record BoardValue(BoardBuff Buff, IBoard Board)
+			: BaseBoardValue(Buff, Board)
+		{
+			public override void Add(in AddBoardContext context)
+			{
+				base.Add(context);
+				Buff._values.Add(context);
+			}
+		}
+	}
+}
---
+++ Blackboard/Editor/BaseBoardKeyEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BB
+{
+	[CustomEditor(typeof(BaseBoardKey))]
+	public sealed class BaseBoardKeyEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+			using (LayoutUtils.Horizontal)
+			{
+				//if(GUILayout.Button(""))
+			}
+		}
+	}
+}
