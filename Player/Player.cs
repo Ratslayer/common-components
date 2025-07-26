1c3da68531018787c70857f9c29cddb2fb329bc7
Arseni <arseni.natapov@gmail.com>
2025-07-26 12:13:40 +0300

    Added Player

---
+++ Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using BB.Di;
+using UnityEngine;
+namespace BB
+{
+	public sealed record Player : EntityVariable<Player>
+	{
+		public Vector3 Position => Has(out Root root) ? root.Position : Vector3.zero;
+	}
+	public abstract record SubscribeToEntityVariableEventSystem<TVariable, TEvent>(TVariable Var)
+		where TVariable: EntityVariable<TVariable>
+	{
+		protected abstract void OnVariableEvent(TEvent e);
+		[OnSpawn]
+		void OnSpawn()
+		{
+			OnEventSubscribe(default);
+			Var._event.Subscribe(OnEventSubscribe);
+		}
+		[OnDespawn]
+		void OnDespawn()
+		{
+			if (Var.Value.Has(out IEvent<TEvent> e))
+				e.Unsubscribe(OnVariableEvent);
+			Var._event.Unsubscribe(OnEventSubscribe);
+		}
+		void OnEventSubscribe(TVariable _)
+		{
+			if (Var.PreviousValue.Has(out IEvent<TEvent> e))
+				e.Unsubscribe(OnVariableEvent);
+			if(Var.Value.Has(out e))
+				e.Subscribe(OnVariableEvent);
+		}
+	}
+}
---
+++ Player/SpawnAndAttachToPlayer.cs
@@ -0,0 +1,17 @@
+﻿using BB.Di;
+
+namespace BB
+{
+	public sealed record AttachToPlayerOnEnable(Player Player) : EntitySystem
+	{
+		[OnEnable, OnEvent(typeof(Player))]
+		void OnEnable() => Entity.AttachTo(Player);
+	}
+	public sealed record SpawnAndAttachToPlayer(
+		IEntityInstaller Installer,
+		Player Player) : EntitySystem
+	{
+		[OnSpawn, OnEvent(typeof(Player))]
+		void Attach() => Installer.SpawnAndAttachTo(Player);
+	}
+}
