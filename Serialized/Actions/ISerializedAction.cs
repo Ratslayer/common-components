a2b587651953b98b43a761e807cdbc54ad8ae9e4
Arseni <arseni.natapov@gmail.com>
2025-09-12 20:41:19 +0300

    Added spawn serialized action

--- Serialized/Actions/ISerializedAction.cs
+++ Serialized/Actions/ISerializedAction.cs
@@ -1,6 +1,4 @@
 ﻿using Cysharp.Threading.Tasks;
-using System;
-using UnityEngine;
 
 namespace BB.Serialized.Actions
 {
@@ -27,56 +25,5 @@ namespace BB.Serialized.Actions
 
 	public static class SerializedActionExtensions
 	{
-	}
-	[Serializable]
-	public sealed class SerializedActions
-	{
-		[SerializeField]
-		SerializedTriggers _triggers = new();
-		[SerializeReference]
-		ISerializedAction[] _actions = { };
-		[SerializeReference]
-		ISerializedActionCondition[] _conditions = { };
-		[SerializeReference]
-		ISerializedAction[] _failedActions = { };
-
-		public IDisposable Subscribe(Entity entity)
-		{
-			var subscription = _triggers.CreateSubscription(new()
-			{
-				Entity = entity,
-				Action = () => Invoke().Forget()
-			});
-
-			subscription.Subscribe();
-
-			return subscription;
-
-			async UniTaskVoid Invoke()
-			{
-				var success = true;
-				foreach (var condition in _conditions)
-					if (condition?.CanInvoke(new() { Entity = entity }) is false)
-					{
-						success = false;
-						break;
-					}
-				var context = new SerializedSceneActionContext
-				{
-					Entity = entity
-				};
-				var actions = success ? _actions : _failedActions;
-				foreach (var action in actions)
-					if (action is ISerializedActionSync sync)
-						sync.Invoke(context);
-					else if (action is ISerializedActionAsync async)
-					{
-						var task = async.Invoke(context);
-						if (async.WaitForExecution)
-							await task;
-						else task.Forget();
-					}
-			}
-		}
 	}
 }
---
+++ Serialized/Actions/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace BB.Serialized.Actions
+{
+	[Serializable]
+	public sealed class Spawn : ISerializedActionSync
+	{
+		public GameObject _prefab;
+		public Transform _location;
+		public void Invoke(in SerializedSceneActionContext context)
+		{
+			if (!Validator.GetPooled(context.Entity)
+				.IsAssigned(_prefab, nameof(_prefab))
+				.IsAssigned(_location, nameof(_location))
+				.ValidateAndDispose())
+				return;
+			_prefab.SpawnEntity(_location);
+		}
+	}
+}
