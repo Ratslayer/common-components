ee4512015bcb8c698fd2d2117c3ec5e334aa498b
Arseni <arseni.natapov@gmail.com>
2025-10-12 11:14:53 +0300

    Update GameActionContextExtensions.cs

--- GameActions/GameActionContextExtensions.cs
+++ GameActions/GameActionContextExtensions.cs
@@ -1,33 +1,37 @@
 ﻿using UnityEngine;
 namespace BB
 {
-	public static class GameActionContextExtensions
-	{
-		public static bool TryExecuteWithMessage(this GameAction action, Entity entity, Vector3 position)
-		{
-			return action.TryExecute(new() { Entity = entity }, OnEnd);
-			void OnEnd(GameActionContext context)
-				=> PublishMessage(context, position);
-		}
-		public static void ExecuteWithMessage(this GameAction action, Entity entity, Vector3 position)
-		{
-			action.Execute(new() { Entity = entity }, OnEnd);
-			void OnEnd(GameActionContext context)
-				=> PublishMessage(context, position);
-		}
-		public static GameAction AddBoardValue(this GameAction action, IBoardKey key, double value)
-		=> action.Add(AddConstBoardValueAction.GetPooled(key, value));
-		public static GameAction AddBoardValue(this GameAction action, IBoardKey key, IBoardKey otherKey, double multiplier = 1)
-			=> action.Add(AddOtherBoardValueAction.GetPooled(key, otherKey, multiplier));
-		public static GameAction Publish<TEvent>(this GameAction action, TEvent e, IEvent<TEvent> publisher = null)
-			=> action.Add(PublishEventAction<TEvent>.GetPooled(e, publisher));
-		private static void PublishMessage(GameActionContext context, Vector3 position)
-		{
-			if (!context.Entity.Has(out IEvent<ShowHintEvent> showHint))
-				return;
-			foreach (var message in context.Messages)
-				showHint.Publish(new(message.Text, position));
-		}
-	}
+    public static class GameActionContextExtensions
+    {
+        //public static bool TryExecuteWithMessage(this GameAction action, Entity entity, Vector3 position)
+        //{
+        //    return action.TryExecute(new() { Entity = entity }, OnEnd);
+        //    void OnEnd(GameActionContext context)
+        //        => PublishMessage(context, position);
+        //}
+        //public static void ExecuteWithMessage(this GameAction action, Entity entity, Vector3 position)
+        //{
+        //    action.Execute(new() { Entity = entity }, OnEnd);
+        //    void OnEnd(GameActionContext context)
+        //        => PublishMessage(context, position);
+        //}
+        public static GameAction AddBoardValue(this GameAction action, IBoardKey key, double value)
+        => action.Add(AddConstBoardValueAction.GetPooled(key, value));
+        public static GameAction AddBoardValue(this GameAction action, IBoardKey key, IBoardKey otherKey, double multiplier = 1)
+            => action.Add(AddOtherBoardValueAction.GetPooled(key, otherKey, multiplier));
+        public static GameAction Publish<TEvent>(this GameAction action, TEvent e, IEvent<TEvent> publisher = null)
+            => action.Add(PublishEventAction<TEvent>.GetPooled(e, publisher));
+        private static void PublishMessage(GameActionContext context, PositionAdapter position)
+        {
+            if (!context.Entity.Has(out IEvent<ShowHintEvent> showHint))
+                return;
+            foreach (var message in context.Messages)
+                showHint.Publish(new()
+                {
+                    Text = message.Text,
+                    Position = position
+                });
+        }
+    }
 
 }
