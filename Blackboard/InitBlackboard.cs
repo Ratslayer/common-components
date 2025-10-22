e065efa3391f0189363435c10286d90e2d1a5d86
Arseni <arseni.natapov@gmail.com>
2025-10-22 18:28:30 +0300

    Board resources now set to max on spawn

--- Blackboard/InitBlackboard.cs
+++ Blackboard/InitBlackboard.cs
@@ -9,5 +9,18 @@
         {
             Values.Add(new() { Board = Board });
         }
+        [OnPostSpawn]
+        void OnPostSpawn()
+        {
+            foreach (var key in Board.Keys)
+                if (key is IResourceBoardKey resource
+                    && resource.SetToMaxOnSpawn)
+                    new AddBoardContext
+                    {
+                        Board = Board,
+                        Key = key,
+                        Value = 1e100
+                    }.Add();
+        }
     }
 }
