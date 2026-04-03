406e767fe33bc30c844fc5043e5919792c1ffdbe
Arseni <arseni.natapov@gmail.com>
2026-04-03 22:34:08 +0300

    Removed effect amanager

--- EffectManager/EffectExtensions.cs
+++ EffectManager/EffectExtensions.cs
@@ -1,12 +1,12 @@
-﻿using BB.Di;
-namespace BB
-{
-	public static class EffectExtensions
-	{
-		public static void BindEffectManagers(
-			this IDiContainer container)
-		{
-			container.Service<IMaterialEffectManager, MaterialEffectManager>();
-		}
-	}
-}
+﻿// using BB.Di;
+// namespace BB
+// {
+// 	public static class EffectExtensions
+// 	{
+// 		public static void BindEffectManagers(
+// 			this IDiContainer container)
+// 		{
+// 			container.Service<IMaterialEffectManager, MaterialEffectManager>();
+// 		}
+// 	}
+// }
--- EffectManager/MaterialEffectManager.cs
+++ EffectManager/MaterialEffectManager.cs
@@ -1,50 +1,49 @@
-﻿using BB.Di;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace BB
-{
-	public sealed record MaterialEffectManager
-		: IMaterialEffectManager
-	{
-		readonly Dictionary<Renderer, MaterialEffectContainer> _effects = new();
-		readonly List<IMaterialEffectContainer> _dirtyContainers = new();
-		
-		public void Clear(Renderer renderer)
-		{
-			if (_effects.TryGetValue(renderer, out var effects))
-			{
-				_effects[renderer] = default;
-				effects.Dispose();
-			}
-		}
-
-		public IMaterialEffectContainer GetContainer(Renderer renderer)
-		{
-			if (!_effects.TryGetValue(renderer, out var effects))
-			{
-				effects = MaterialEffectContainer.GetPooled();
-				effects._renderer = renderer;
-				effects._manager = this;
-
-				_effects[renderer] = effects;
-			}
-			return effects;
-		}
-
-		public void SetDirty(IMaterialEffectContainer container)
-		{
-			_dirtyContainers.AddUnique(container);
-		}
-
-		[OnEvent]
-		void OnLateUpdate(LateUpdateEvent _)
-		{
-			if (_dirtyContainers.Count == 0)
-				return;
-			foreach (var container in _dirtyContainers)
-				container.Update();
-			_dirtyContainers.Clear();
-		}
-	}
-}
+﻿// using System.Collections.Generic;
+// using UnityEngine;
+//
+// namespace BB
+// {
+// 	public sealed record MaterialEffectManager
+// 		: IMaterialEffectManager
+// 	{
+// 		readonly Dictionary<Renderer, MaterialEffectContainer> _effects = new();
+// 		readonly List<IMaterialEffectContainer> _dirtyContainers = new();
+// 		
+// 		public void Clear(Renderer renderer)
+// 		{
+// 			if (_effects.TryGetValue(renderer, out var effects))
+// 			{
+// 				_effects[renderer] = default;
+// 				effects.Dispose();
+// 			}
+// 		}
+//
+// 		public IMaterialEffectContainer GetContainer(Renderer renderer)
+// 		{
+// 			if (!_effects.TryGetValue(renderer, out var effects))
+// 			{
+// 				effects = MaterialEffectContainer.GetPooled();
+// 				effects._renderer = renderer;
+// 				effects._manager = this;
+//
+// 				_effects[renderer] = effects;
+// 			}
+// 			return effects;
+// 		}
+//
+// 		public void SetDirty(IMaterialEffectContainer container)
+// 		{
+// 			_dirtyContainers.AddUnique(container);
+// 		}
+//
+// 		[OnEvent]
+// 		void OnLateUpdate(LateUpdateEvent _)
+// 		{
+// 			if (_dirtyContainers.Count == 0)
+// 				return;
+// 			foreach (var container in _dirtyContainers)
+// 				container.Update();
+// 			_dirtyContainers.Clear();
+// 		}
+// 	}
+// }
